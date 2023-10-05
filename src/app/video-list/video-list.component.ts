4dab68d31718322a6acd20231a415e17cfe5b575
Liviu <liviutoma946@gmail.com>
2023-10-05 22:52:42 +0300

    preparing data to be displayed on page, added datePipe to format data

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VideoService } from '../video.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.sass'],
 })
-export class VideoListComponent {
-  videos: any[] = [
-    { title: 'Video 1', description: 'Description 1' },
-    { title: 'Video 2', description: 'Description 2' },
-    { title: 'Video 3', description: 'Description 3' },
-  ];
-
+export class VideoListComponent implements OnInit {
+  videos: any[] = [];
   gridView: boolean = true;
+
+  constructor(private videoService: VideoService, private datePipe: DatePipe) {}
+
+  ngOnInit() {
+    this.videoService.getVideos().subscribe((data: any) => {
+      console.log(data);
+      this.videos = data.map((video: any) => ({
+        ...video,
+        createdDate: this.datePipe.transform(video.createdDate, 'MM/dd/yyy'),
+      }));
+    });
+  }
 
   toggleView() {
     this.gridView = !this.gridView;
