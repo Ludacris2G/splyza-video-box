0d7d8ee4f53df826aa20cc83cdef6bc9ef9ec2f0
Liviu <liviutoma946@gmail.com>
2023-10-06 20:53:24 +0300

    navbar user profile, fetching and styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  userData: any;
 
+  constructor(private userService: UserService) {}
+
+  // fetch user data
+  ngOnInit() {
+    this.userService.getUser().subscribe((data: Object) => {
+      this.userData = data;
+    });
+  }
 }
