e0695a092e7a4ed5cfca0a26a77323b50706aa3a
Liviu <liviutoma946@gmail.com>
2023-10-07 00:13:16 +0300

    added routes for videos and video details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoDetailsComponent } from './components/video-details/video-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/videos', pathMatch: 'full' },
+  { path: 'videos', component: VideoListComponent },
+  { path: 'videos/:videoId', component: VideoDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
