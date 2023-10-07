22fe48b6b08dac9e7db18d14fda3da87b5b947b4
Liviu <liviutoma946@gmail.com>
2023-10-07 14:39:10 +0300

    added video reactions to video-details component. Fetched rections, added styling, implemented conditional displaying, styling, timeframe convertion

--- src/app/components/video-details/video-details.component.ts
+++ src/app/components/video-details/video-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VideoDetailsService } from 'src/app/services/video-details.service';
 import { UserService } from 'src/app/services/user.service';
+import { VideoReactionsService } from 'src/app/services/video-reactions.service';
 
 @Component({
   selector: 'app-video-details',
@@ -14,16 +15,21 @@ export class VideoDetailsComponent implements OnInit {
   titleChanged: boolean = false;
   userIsAuthor: boolean = false;
   userData: any;
+  videoId: any;
+  videoReactions: any;
 
   constructor(
     private route: ActivatedRoute,
     private videoService: VideoDetailsService,
-    private userService: UserService
+    private userService: UserService,
+    private reactionsService: VideoReactionsService
   ) {}
 
   ngOnInit() {
+    // fetch video details
     this.route.paramMap.subscribe((params) => {
       const videoId = params.get('videoId');
+      this.videoId = videoId;
       if (videoId) {
         this.videoService.getVideoDetails(videoId).subscribe(
           (data: Object) => {
@@ -40,16 +46,44 @@ export class VideoDetailsComponent implements OnInit {
       }
     });
 
+    this.reactionsService.getVideoReactions(this.videoId).subscribe(
+      (reactions: any) => {
+        console.log(reactions);
+        this.videoReactions = reactions;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    // check logged user
     this.userService.getUser().subscribe((data: Object) => {
       this.userData = data;
     });
   }
 
+  // check if user is the author
   isUserAuthor(authorId: string): boolean {
     return this.userData && authorId === this.userData.id;
   }
 
   onTitleChange() {
     this.titleChanged = true;
+  }
+
+  formatTimeframe(timeframe: number): string {
+    console.log(timeframe);
+    const hours = Math.floor(timeframe / 3600);
+    const minutes = Math.floor((timeframe % 3600) / 60);
+    const seconds = Math.floor(timeframe % 60);
+
+    const formattedTimeframe = `${this.padZero(hours)}:${this.padZero(
+      minutes
+    )}:${this.padZero(seconds)}`;
+    return formattedTimeframe;
+  }
+
+  padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
   }
 }
