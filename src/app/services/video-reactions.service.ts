6cdde8190d704913f17a115b719436b72166d5f9
Liviu <liviutoma946@gmail.com>
2023-10-07 16:32:24 +0300

    added reaction POST

--- src/app/services/video-reactions.service.ts
+++ src/app/services/video-reactions.service.ts
@@ -8,9 +8,15 @@ import { environment } from 'src/environments/environment.development';
 export class VideoReactionsService {
   constructor(private http: HttpClient) {}
 
+  // GET video reactions
   getVideoReactions(videoId: string) {
     return this.http.get(
       `${environment.backend}/api/videos/${videoId}/reactions`
     );
+  }
+
+  // POST video reaction
+  addReaction(reaction: any) {
+    return this.http.post(`${environment.backend}/api/videos/${reaction.videoId}/reactions`,reaction)
   }
 }
