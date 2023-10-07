76c81e18a7c70940980ab64bfabecf949cc34d88
Liviu <liviutoma946@gmail.com>
2023-10-07 16:32:06 +0300

    added video title patch

--- src/app/services/video-details.service.ts
+++ src/app/services/video-details.service.ts
@@ -6,9 +6,21 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root',
 })
 export class VideoDetailsService {
-  constructor( private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
+  // GET video details
   getVideoDetails(videoId: string) {
     return this.http.get(`${environment.backend}/api/videos/${videoId}`);
   }
+
+  // PATCH video title
+  updateVideoTitle(videoId: string, title: string) {
+    const url = `${environment.backend}/api/videos/${videoId}`;
+    const body = {
+      title,
+    };
+    return this.http.patch(url, body);
+  }
+
+
 }
